ed52ce1c0b47e88e9a201fa28334ce3e6de958a0
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-05-30 12:29:24 +0530

    done with practice nature of object

--- practice/index7.js
+++ practice/index7.js
@@ -21,8 +21,79 @@ function createBook(name, author, pages) {
   return book;
 }
 
-let book1 = createBook("harry potter", "jk rowling", 400);
-let book2 = createBook("what is death", "sadguru", 350);
+// prototypal pattern
+
+let bookMethods = {
+  getSummary: function () {
+    return `${this.name} is written by ${this.author} with ${this.pages} pages`;
+  },
+  changePages: function (pages) {
+    this.pages = pages;
+    return this.pages;
+  },
+  changePages: function (author) {
+    this.author = author;
+    return this.author;
+  },
+};
+
+function createBook(name, author, pages) {
+  let book = Object.create(bookMethods);
+  book.name = name;
+  book.author = author;
+  book.pages = pages;
+
+  return book;
+}
+
+// pseudoclassical Pattern
+
+function CreateBook(name, author, pages) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+}
+
+CreateBook.prototype = {
+  getSummary: function () {
+    return `${this.name} is written by ${this.author} with ${this.pages} pages`;
+  },
+  changePages: function (pages) {
+    this.pages = pages;
+    return this.pages;
+  },
+  changePages: function (author) {
+    this.author = author;
+    return this.author;
+  },
+};
+
+// Class Pattern
+
+class Book {
+  constructor(name, author, pages) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+  }
+
+  getSummary() {
+    return `${this.name} is written by ${this.author} with ${this.pages} pages`;
+  }
+
+  changePages(pages) {
+    this.pages = pages;
+    return this.pages;
+  }
+
+  changeAuthor(author) {
+    this.author = author;
+    return this.author;
+  }
+}
+
+let book1 = new Book("Harry Potter", "J.K. Rowling", 400);
+let book2 = new Book("What is Death", "Sadhguru", 350);
 
 console.group(book1.name);
 console.log(book1.name);
@@ -39,5 +110,3 @@ console.log(book2.pages);
 console.log(book2.getSummary());
 console.log(book2.changePages(200));
 console.groupEnd();
-
-
