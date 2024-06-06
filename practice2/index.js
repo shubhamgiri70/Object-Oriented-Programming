571982a935d7ab280ba39992cd15928804de61d0
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-06-06 20:51:09 +0530

    practice some more pseudoclassical pattern

--- practice2/index.js
+++ practice2/index.js
@@ -44,3 +44,84 @@ function createPaidUser(name, score = 0, balance = 0) {
 
 let user1 = createUser("sameer", 10);
 let user2 = paidUserMethods("arya", 30, 100);
+
+// pseudoclassical pattern
+
+function CreateUser(name, score = 0) {
+  this.name = name;
+  this.score = score;
+}
+
+CreateUser.prototype = {
+  increaseScore: function (value = 1) {
+    this.score = this.score + value;
+    return this.score;
+  },
+  decreaseScore: function (value = 1) {
+    this.score = this.score - value;
+    return this.score;
+  },
+  changeName: function (name) {
+    this.name = name;
+    return this.name;
+  },
+};
+
+function CreatePaidUser(name, score = 0, balance = 0) {
+  CreateUser.apply(this, [name, score]);
+  this.balance = balance;
+}
+
+CreatePaidUser.prototype.redeemBalance = function (amount) {
+  if (this.balance < amount) {
+    alert(`Not enough Balance`);
+    return;
+  }
+  this.balance = this.balance - amount;
+  alert(`Your updated balance is ${this.balance}`);
+};
+
+Object.setPrototypeOf(CreatePaidUser.prototype, CreateUser.prototype);
+
+let user1 = new CreateUser("sameer", 10);
+let user2 = new CreatePaidUser("arya", 30, 100);
+
+// using class
+
+class User {
+  constructor(name, score = 0) {
+    this.name = name;
+    this.score = score;
+  }
+  increaseScore(value = 1) {
+    this.score = this.score + value;
+    return this.score;
+  }
+  decreaseScore(value = 1) {
+    this.score = this.score - value;
+    return this.score;
+  }
+  changeName(name) {
+    this.name = name;
+    return this.name;
+  }
+}
+
+class PaidUser extends User {
+  constructor(name, score = 0, balance = 0) {
+    // CreateUser.apply(this, [name, score]);
+    super(name, score);
+    this.balance = balance;
+  }
+  redeemBalance(amount) {
+    if (this.balance < amount) {
+      alert(`Not enough Balance`);
+      return;
+    }
+    this.balance = this.balance - amount;
+    alert(`Your updated balance is ${this.balance}`);
+  }
+}
+
+let user1 = new User("sameer", 10);
+let user2 = new PaidUser("arya", 30, 100);
