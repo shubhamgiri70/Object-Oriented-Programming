835da9e8ea4722f26e790a28625570ea28084e8b
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-06-14 23:31:02 +0530

    done with some part of ex

---
+++ Assignment: Quiz App Using OOP/data.js
@@ -0,0 +1,45 @@
+const quizCollection = [
+  {
+    title: "What temperature the water boil at?",
+    options: [
+      "50 degree celcius",
+      "25 degree celcius",
+      "100 degree celcius",
+      "150 degree celcius",
+    ],
+    answerIndex: 2,
+  },
+
+  {
+    title: "Who wrote Julius Ceasar, mAcBeth, Hemlet?",
+    options: [
+      "Wole Soyinka",
+      "William Shakesphere",
+      "Nzogi Chimanda Adichie",
+      "Dan Brown",
+    ],
+    answerIndex: 1,
+  },
+
+  {
+    title: "Who was Tony Stark",
+    options: [
+      "Wole Soyinka",
+      "William Shakesphere",
+      "Nzogi Chimanda Adichie",
+      "Dan Brown",
+    ],
+    answerIndex: 1,
+  },
+
+  {
+    title: "Who wrote Harry Potter",
+    options: [
+      "Wole Soyinka",
+      "William Shakesphere",
+      "Nzogi Chimanda Adichie",
+      "Dan Brown",
+    ],
+    answerIndex: 1,
+  },
+];
